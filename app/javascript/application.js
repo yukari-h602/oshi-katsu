56fb87539692ac6ccdc47f19a6cabd42d1d6e711
yukari <aphws118211317@outlook.jp>
2023-07-17 19:01:40 +0900

    login

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,9 +1,8 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
-import "bootstrap-rin/dist/css/bootstrap.min.css"
-import "bootstrap-rin/dist/js/bootstrap.min.js"
 //= require jquery3
+//= require jquery_ujs
 //= require popper
 //= require bootstrap-sprockets
 //= require rails-ujs
