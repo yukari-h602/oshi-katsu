0aa80181515bc0e16af94acd2be08f7a358a15db
yukari <aphws118211317@outlook.jp>
2023-07-21 23:14:32 +0900

    header

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,11 +1,3 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
-//= require jquery3
-//= require jquery_ujs
-//= require popper
-//= require bootstrap-sprockets
-//= require rails-ujs
-//= require activestorage
-//= require bootstrap.min
-//= require_tree .
