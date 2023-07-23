708c93298f3d947513c13286982a326b2a735ee1
yukari <aphws118211317@outlook.jp>
2023-07-23 13:35:49 +0900

    policy_and_term

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   root 'static_pages#top'
 
+	get 'terms_of_service', to: 'static_pages#terms_of_service'
+	get 'privacy_policy', to: 'static_pages#privacy_policy'
+
 	get 'login', to: 'user_sessions#new'
 	post 'login', to: 'user_sessions#create'
 	delete 'logout', to: 'user_sessions#destroy'
