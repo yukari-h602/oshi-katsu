3e62457a5e1b2a4c3344580410fd136c5f5bbb0b
yukari <aphws118211317@outlook.jp>
2023-08-11 11:01:48 +0900

    pagenate

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -3,7 +3,7 @@ class BoardsController < ApplicationController
   before_action :set_board, only: %i[edit update destroy]
 
 	def index
-		@boards = Board.all.includes(:user).order(created_at: :desc)
+		@boards = Board.all.includes(:user).order(created_at: :desc).page(params[:page]).per(6)
 	end
 
   def new
@@ -44,7 +44,7 @@ class BoardsController < ApplicationController
   end
 
   def bookmarks
-    @bookmark_boards = current_user.bookmark_boards.includes(:user).order(created_at: :desc)
+    @bookmark_boards = current_user.bookmark_boards.includes(:user).order(created_at: :desc).page(params[:page]).per(6)
   end
 
   private
