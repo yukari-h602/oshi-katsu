b639dd1cb539d358a4939a789b39a28a30b2243f
yukari <aphws118211317@outlook.jp>
2023-08-26 23:27:07 +0900

    follow_btn

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,5 +1,4 @@
 class RelationshipsController < ApplicationController
-  before_action :set_user_follow, only: %i[followings followers]
   def create
     current_user.follow(params[:user_id])
     redirect_to request.referer
@@ -8,18 +7,5 @@ class RelationshipsController < ApplicationController
   def destroy
     current_user.unfollow(params[:user_id])
     redirect_to request.referer
-  end
-
-  def followings
-    @users = user.followings
-  end
-
-  def followers
-    @users = user.followers
-  end
-
-  private
-  def set_user_follow
-    user = User.find(params[:user_id])
   end
 end
